4ccd5da33415378cadf463371597283a0bd1c9b6
safirasn21 <safirasn021@gmai.com>
2024-11-23 12:32:05 +0700

    import dan export controller untuk login, register, home, profile, dan pupuk

---
+++ src/controllers/index.js
@@ -0,0 +1,14 @@
+// index.js
+const login = require("./controller-login");
+const register = require("./controller-register");
+const home = require("./controller-home");
+const profile = require("./controller-profile");
+const contact = require("./controller-pupuk");
+
+module.exports = {
+    login,
+    register,
+    home,
+    profile,
+    pupuk,
+};
