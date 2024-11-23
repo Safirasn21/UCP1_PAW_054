5d36d17f0f6c82e3d3670212d13ad9c4828ff349
safirasn21 <safirasn021@gmai.com>
2024-11-23 12:40:41 +0700

    menambahkan fitur autentikasi login

---
+++ routes/router-login.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const db = require("../../configs/database");
+
+
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const query = "SELECT * FROM users WHERE email = ?";
+    db.query(query, [email], async (err, results) => {
+        if (err || results.length === 0) {
+            res.send("User not found");
+        } else {
+            const user = results[0];
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (isMatch) {
+                req.session.userId = user.id; 
+                res.redirect("/dashboard"); 
+            } else {
+                res.send("Incorrect password");
+            }
+        }
+    });
+});
+
+module.exports = router;
